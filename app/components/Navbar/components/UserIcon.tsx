117691671d9911c527900ce873e728f174366179
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-16 12:06:32 +0100

    fix: avatar_url

--- app/components/Navbar/components/UserIcon.tsx
+++ app/components/Navbar/components/UserIcon.tsx
@@ -3,27 +3,20 @@ import Image from "next/image"
 
 import { BiUserCircle } from "react-icons/bi"
 import { LogoutButton } from "./LogoutButton"
+import { TUser } from "@/interfaces/IUser"
 
 interface UserIconProps {
-  user: {
-    avatarUrl: string | null
-    created_at: string
-    email: string
-    email_confirmed_at: string | null
-    id: string
-    role: string
-    username: string
-  } | null
+  user: TUser
 }
 
 export default function UserIcon({ user }: UserIconProps) {
   return (
     <div className="flex flex-col gap-y-2 justify-center items-center">
       {user ? (
-        user.avatarUrl ? (
+        user.avatar_url ? (
           <Image
             className="rounded-full cursor-pointer"
-            src={user.avatarUrl}
+            src={user.avatar_url}
             alt="profile_picture_url"
             width={32}
             height={32}
