d739fa39d2cdf74086f01c24faba2cc4502bb498
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-16 18:07:05 +0100

    style: text-danger from <p> removed

--- app/error/EmailLinkInvalidOrExpired.tsx
+++ app/error/EmailLinkInvalidOrExpired.tsx
@@ -14,8 +14,8 @@ export function EmailLinkInvalidOrExpired() {
       <div className="h-[35vh] w-full bg-brand" />
       <p className="text-danger">Email link is invalid or has expired</p>
       <p>In short, there is a mistake here</p>
-      <p className="text-danger">Try again and use link that you become ASAP</p>
-      <p className="text-danger">Also don&apos;t use link that you already used</p>
+      <p>Try again and use link that you become ASAP</p>
+      <p>Also don&apos;t use link that you already used</p>
     </div>
   )
 }
