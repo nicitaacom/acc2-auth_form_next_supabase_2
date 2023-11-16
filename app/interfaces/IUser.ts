c86644dc0226b184b3225b6eb4382b906cfb9a46
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-16 12:05:43 +0100

    upd: interface for IUser added

---
+++ app/interfaces/IUser.ts
@@ -0,0 +1,9 @@
+export type TUser = {
+  avatar_url: string | null
+  created_at: string
+  email: string
+  email_confirmed_at: string | null
+  id: string
+  role: string
+  username: string
+} | null
